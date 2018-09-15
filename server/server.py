4d5540299e8fbf1f42d9b2d7f1449c2983fcea2f
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2018-09-15 11:31:02 +0200

    setting up workable model for the server side

--- server/server.py
+++ server/server.py
@@ -6,14 +6,15 @@ import random
 
 members = ['ali', 'fredrico', 'gabriel', 'sergei']
 
-class healthData:
-    customer_name = random.choice(members)
-    timestamp = datetime.datetime.utcnow()
-    heartbeat = randint(35, 150)
+# cannot figure out why the object doesn't get destroyed at the end of function call
+# class healthData:
+#     customer_name = random.choice(members)
+#     timestamp = datetime.datetime.utcnow()
+#     heartbeat = randint(35, 150)
 
-    @staticmethod
-    def customer_health_data():
-        return jsonify(random.choice(members), datetime.datetime.utcnow(), randint(35, 150))
+#     @staticmethod
+#     def customer_health_data():
+#         return jsonify(random.choice(members), datetime.datetime.utcnow(), randint(35, 150))
 
 app = Flask(__name__)
 
@@ -23,4 +24,4 @@ def index():
 
 @app.route("/health")
 def health():
-	return (healthData().customer_health_data())
+	return jsonify({ 'customer_name' : random.choice(members), 'timestamp' : datetime.datetime.utcnow(), 'heartbeat' :  randint(35, 150) })
