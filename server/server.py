75239d89bdb21cbc8ab38cbaf779888482dc30ee
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2018-09-15 16:23:58 +0200

    Added POST function to the swagger API

--- server/server.py
+++ server/server.py
@@ -1,8 +1,6 @@
 from flask import Flask, jsonify
-from random import randint, random
-import datetime
-
-import random
+from random import randint
+import datetime, urllib, json, codecs, random, time, requests
 
 members = ['ali', 'fredrico', 'gabriel', 'sergei']
 
@@ -22,6 +20,25 @@ app = Flask(__name__)
 def index():
     return "Index Page"
 
+def postClient():
+	url = 'http://8732a407.ngrok.io/api/InsuredClient'
+	r = requests.post(url, json={'name': 'aliabbasjaffri', 'id' : 1500, 'age' : 25})
+	print(r.status_code)
+
+def getInsurer():
+	url = 'http://8732a407.ngrok.io/api/Insurance/123'
+	response = urllib.urlopen(url)
+	reader = codecs.getreader("utf-8")
+	return json.load(reader(response))
+
+def getClient():
+	url = 'http://8732a407.ngrok.io/api/InsuredClient/123'
+	response = urllib.urlopen(url)
+	reader = codecs.getreader("utf-8")
+	return json.load(reader(response))
+
 @app.route("/health")
-def health():
-	return jsonify({ 'customer_name' : random.choice(members), 'timestamp' : datetime.datetime.utcnow(), 'heartbeat' :  randint(35, 150) })
+def postHealthMeasurement():
+	insurer = getInsurer()
+	return jsonify({ 'heartRate': randint(60, 150), 'viewer' : insurer,
+	 'owner' : getClient(), 'id' : int(time.time()), 'timestamp' : datetime.datetime.utcnow()})
