0f4ef8a759fe168f03a0fb1dc53a025bcc7b6d28
Gabriel Fior <gabrielfior@Gabriels-MacBook-Pro.local>
2018-09-15 17:26:34 +0200

    added single transaction example

--- hyperledger/insurance-network/lib/logic.js
+++ hyperledger/insurance-network/lib/logic.js
@@ -4,7 +4,25 @@
  * @transaction
  */
 async function tradeCommodity(trade) {
+    console.log(trade);
     trade.healthMeasurement = trade.healthMeasurement;
     let assetRegistry = await getAssetRegistry('org.example.biznet.HealthMeasurement');
     await assetRegistry.update(trade.commodity);
+}
+
+/**
+ * Sample transaction processor function.
+ * @param {org.example.biznet.SampleTransaction} tx The sample transaction instance.
+ * @transaction
+ */
+async function sampleTransaction(tx) {
+    // Save the old value of the asset.
+    let oldValue = tx.asset.value;
+    // Update the asset with the new value.
+    tx.asset.value = tx.newValue;
+    // Get the asset registry for the asset.
+    let assetRegistry = await getAssetRegistry('org.example.biznet.SampleAsset');
+    // Update the asset in the asset registry.
+    await assetRegistry.update(tx.asset);
+
 }
