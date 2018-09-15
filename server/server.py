e548f586ff2842dbf852c12d06ace7e7d4822694
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2018-09-15 19:19:47 +0200

    API set up to post healthmeasurement data

--- server/server.py
+++ server/server.py
@@ -4,16 +4,6 @@ import datetime, urllib, json, codecs, random, time, requests
 
 members = ['ali', 'fredrico', 'gabriel', 'sergei']
 
-# cannot figure out why the object doesn't get destroyed at the end of function call
-# class healthData:
-#     customer_name = random.choice(members)
-#     timestamp = datetime.datetime.utcnow()
-#     heartbeat = randint(35, 150)
-
-#     @staticmethod
-#     def customer_health_data():
-#         return jsonify(random.choice(members), datetime.datetime.utcnow(), randint(35, 150))
-
 app = Flask(__name__)
 
 @app.route("/")
@@ -24,21 +14,33 @@ def postClient():
 	url = 'http://8732a407.ngrok.io/api/InsuredClient'
 	r = requests.post(url, json={'name': 'aliabbasjaffri', 'id' : 1500, 'age' : 25})
 	print(r.status_code)
+	return str(r.status_code)
 
 def getInsurer():
+	insurers = []
 	url = 'http://8732a407.ngrok.io/api/Insurance/123'
 	response = urllib.urlopen(url)
 	reader = codecs.getreader("utf-8")
-	return json.load(reader(response))
+	# return json.load(reader(response))
+	insurers.append(json.load(reader(response))['id'])
+	return insurers
 
 def getClient():
 	url = 'http://8732a407.ngrok.io/api/InsuredClient/123'
 	response = urllib.urlopen(url)
 	reader = codecs.getreader("utf-8")
-	return json.load(reader(response))
+	# return json.load(reader(response))
+	return json.load(reader(response))['id']
 
 @app.route("/health")
 def postHealthMeasurement():
-	insurer = getInsurer()
-	return jsonify({ 'heartRate': randint(60, 150), 'viewer' : insurer,
-	 'owner' : getClient(), 'id' : int(time.time()), 'timestamp' : datetime.datetime.utcnow()})
+
+	print(datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S.000'))
+
+	url = 'http://8732a407.ngrok.io/api/HealthMeasurement'
+	r = requests.post(url, json={ 'heartRate': randint(60, 150), 'numberOfStepsInInterval' : randint(1, 10), 
+		'datetimeBeginStepsInterval' : datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S.000'), 
+		'datetimeEndStepsInterval' : datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S.000'),
+		'id' : str(int(time.time())), 'viewers' : getInsurer(), 'ownerId' : getClient() })
+	print(r.status_code)
+	return str(r.status_code)
