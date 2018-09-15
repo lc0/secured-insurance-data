2b885979997ba09ee26b9b82d922b513dd17a88f
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2018-09-15 11:13:12 +0200

    Setting up flask to post basic personal data at endpoint [localhost] / health

--- server/server.py
+++ server/server.py
@@ -1,6 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify
+from random import randint, random
+import datetime
+
+import random
+
+members = ['ali', 'fredrico', 'gabriel', 'sergei']
+
+class healthData:
+    customer_name = random.choice(members)
+    timestamp = datetime.datetime.utcnow()
+    heartbeat = randint(35, 150)
+
+    @staticmethod
+    def customer_health_data():
+        return jsonify(random.choice(members), datetime.datetime.utcnow(), randint(35, 150))
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return "Index Page"
+§
+@app.route("/health")
+def health():
+	return (healthData().customer_health_data())
